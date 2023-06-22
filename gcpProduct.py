dc9dc9f45d5ea77461fbf2e18d794df80ef6084d
Rhys E <rhys.ellwood@hotmail.com>
2023-06-22 22:21:41 +1000

    Initial Push

---
+++ gcpProduct.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def google_product_scraper():
+    """
+    Scrapes the Google Cloud Platform products page and returns a list of dictionaries containing the product section
+    and its corresponding services.
+
+    Returns:
+        list: A list of dictionaries containing the product section and its corresponding services.
+    """
+    url = "https://cloud.google.com/products#section-5"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    heading_elements = soup.find_all(class_="link-card-grid-section cloud-jump-section")
+
+    data = []
+    for heading_element in heading_elements:
+        heading = heading_element.find(
+            class_="cws-headline--headline-3 link-card-grid-module__headline cws-content-block__headline").text.strip()
+        services = []
+        parent_elements = heading_element.find_all(class_="cws-grid cws-cards cws-cards--compact")
+
+        for parent_element in parent_elements:
+            service_elements = parent_element.find_all(class_="cws-card__inner")
+
+            for service_element in service_elements:
+                headline = service_element.find(class_="cws-headline cws-headline--headline-5").text.strip()
+                description = service_element.find(class_="cws-body").text.strip()
+                service = {
+                    "ProductName": headline,
+                    "Description": description
+                }
+                services.append(service)
+
+        # Exclude irrelevant heading as this just shows products that are featured.
+        if heading not in ["Featured products"]:
+            heading_data = {
+                "Product Section": heading,
+                "Services": services
+            }
+            data.append(heading_data)
+    return data
+
+scraped_data = google_product_scraper()
+
+json_file_path = "GCP_Products.json"
+with open(json_file_path, "w") as json_file:
+    json.dump(scraped_data, json_file, indent=4)
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Google Cloud Platform Product List
+
+![Update GCP Products](https://github.com/REllwood/gcp-product-list/workflows/daily-scraping/badge.svg)
+
+This repository fetches all current GCP products and services from the [GCP Products and Services page](https://cloud.google.com/products/) and stores them in a JSON file daily. A Github Action is used to run the script daily and refresh the data daily.
+
+# Data Format
+
+All data is stored in the GCP_Products.json file. The format is as follows:
+```json
+     "Product Section": "AI and Machine Learning",
+        "Services": [
+            {
+                "ProductName": "Vertex AI",
+                "Description": "Unified platform for training, hosting, and managing ML models."
+            },
+            {
+                "ProductName": "Vertex AI Workbench",
+                "Description": "A single interface for your data, analytics, and machine learning workflow."
+            },
+            {
+                "ProductName": "Vertex Explainable AI",
+                "Description": "Tools and frameworks to understand and interpret your machine learning models."
+            }
+```
+
+I feel this format is the most useful for developers who want to use this data in their own projects.
+
+<Strong>Keys:</Strong>
+- **Product Section:** The section of the GCP Products and Services page the product is listed under.
+- **ProductName:** The name of the GCP product.
+- **Description:** A short description of the product.
